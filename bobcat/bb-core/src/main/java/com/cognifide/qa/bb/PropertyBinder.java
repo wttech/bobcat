637a28e8fb364dbffa864043124a86aaeb3b8437
Szymon Nowak <nowszy94@gmail.com>
2016-09-20 13:01:58 +0200

    #102 CR fixes

--- bobcat/bb-core/src/main/java/com/cognifide/qa/bb/PropertyBinder.java
+++ bobcat/bb-core/src/main/java/com/cognifide/qa/bb/PropertyBinder.java
@@ -19,21 +19,9 @@
  */
 package com.cognifide.qa.bb;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.cognifide.qa.bb.constants.ConfigKeys;
-import com.cognifide.qa.bb.constants.Timeouts;
+import com.cognifide.qa.bb.utils.PropertyUtils;
 import com.google.inject.Binder;
 import com.google.inject.name.Names;
 
@@ -42,10 +30,6 @@ import com.google.inject.name.Names;
  * directly by Bobcat's users.
  */
 public final class PropertyBinder {
-
-  private static final Logger LOG = LoggerFactory.getLogger(PropertyBinder.class);
-
-  private static final String[] PREFIXES = new String[]{"webdriver.", "phantomjs."};
 
   private PropertyBinder() {
   }
@@ -63,86 +47,8 @@ public final class PropertyBinder {
    * @param binder The Binder instance that will store the newly created property bindings.
    */
   public static void bindProperties(Binder binder) {
-    try {
-      String parents = System.getProperty(ConfigKeys.CONFIGURATION_PATHS, "src/main/config");
-      String[] split = StringUtils.split(parents, ";");
-      Properties properties = loadDefaultProperties();
-      for (String name : split) {
-        File configParent = new File(StringUtils.trim(name));
-        loadProperties(configParent, properties);
-      }
-      overrideFromSystemProperties(properties);
-      setSystemProperties(properties);
-      overrideTimeouts(properties);
-      Names.bindProperties(binder, properties);
-      binder.bind(Properties.class).toInstance(properties);
-    } catch (IOException e) {
-      LOG.error("Can't bind properties", e);
-    }
+    Properties properties = PropertyUtils.gatherProperties();
+    Names.bindProperties(binder, properties);
+    binder.bind(Properties.class).toInstance(properties);
   }
-
-  private static void overrideTimeouts(Properties properties) {
-    int big = Integer.parseInt(properties.getProperty(ConfigKeys.TIMEOUTS_BIG));
-    int medium = Integer.parseInt(properties.getProperty(ConfigKeys.TIMEOUTS_MEDIUM));
-    int small = Integer.parseInt(properties.getProperty(ConfigKeys.TIMEOUTS_SMALL));
-    int minimal = Integer.parseInt(properties.getProperty(ConfigKeys.TIMEOUTS_MINIMAL));
-    new Timeouts(big, medium, small, minimal);
-  }
-
-  private static Properties loadDefaultProperties() throws IOException {
-    Properties properties = new Properties();
-    try (InputStream is = PropertyBinder.class.getClassLoader()
-            .getResourceAsStream(ConfigKeys.DEFAULT_PROPERTIES_NAME)) {
-      properties.load(is);
-    } catch (IOException e) {
-      LOG.error("Can't bind default properties", e);
-    }
-    return properties;
-  }
-
-  private static void loadProperties(File file, Properties properties) throws IOException {
-    if (!file.exists()) {
-      return;
-    }
-    if (file.isDirectory()) {
-      for (File child : file.listFiles()) {
-        loadProperties(child, properties);
-      }
-    } else {
-      BufferedReader reader = new BufferedReader(
-              new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-
-      LOG.debug("loading properties from: {} (ie. {})", file, file.getAbsolutePath());
-      try {
-        properties.load(reader);
-      } finally {
-        reader.close();
-      }
-    }
-  }
-
-  private static void overrideFromSystemProperties(Properties properties) {
-    properties.stringPropertyNames().stream().forEach((key) -> {
-      String systemProperty = System.getProperty(key);
-      if (StringUtils.isNotBlank(systemProperty)) {
-        properties.setProperty(key, systemProperty);
-      }
-    });
-  }
-
-  private static void setSystemProperties(Properties properties) {
-    for (String key : properties.stringPropertyNames()) {
-      String systemProperty = System.getProperty(key);
-      if (StringUtils.isNotBlank(systemProperty)) {
-        continue;
-      }
-      for (String prefix : PREFIXES) {
-        if (key.startsWith(prefix)) {
-          System.setProperty(key, properties.getProperty(key));
-          break;
-        }
-      }
-    }
-  }
-
 }
