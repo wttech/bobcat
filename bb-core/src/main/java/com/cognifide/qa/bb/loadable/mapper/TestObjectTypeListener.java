f0625dcc9e926f10cb31b42fe7973ae677782958
Mikolaj Manski <mikolaj.manski@cognifide.com>
2017-03-29 11:38:47 +0200

    #155 - changed logic for registering steps implementation classes to use with loadable components

--- bb-core/src/main/java/com/cognifide/qa/bb/loadable/mapper/TestObjectTypeListener.java
+++ bb-core/src/main/java/com/cognifide/qa/bb/loadable/mapper/TestObjectTypeListener.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2016 Cognifide Ltd..
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.cognifide.qa.bb.loadable.mapper;
 
@@ -19,10 +17,16 @@ import com.google.inject.TypeLiteral;
 import com.google.inject.spi.TypeEncounter;
 import com.google.inject.spi.TypeListener;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.runtime.java.StepDefAnnotation;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.runtime.java.guice.ScenarioScoped;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
 
 public class TestObjectTypeListener implements TypeListener {
 
@@ -36,11 +40,32 @@ public class TestObjectTypeListener implements TypeListener {
 
   private <I> boolean isApplicable(Class<? super I> rawType) {
     boolean result;
-    if (rawType.isAnnotationPresent(RunWith.class) && !rawType.isAnnotationPresent(CucumberOptions.class)) {
+    if (rawType.isAnnotationPresent(RunWith.class)
+        && !rawType.isAnnotationPresent(CucumberOptions.class)) {
       result = true;
     } else {
-      result = rawType.isAnnotationPresent(ScenarioScoped.class);
+      result = isStepsImplementationClass(rawType);
     }
     return result;
   }
+
+  private boolean isStepsImplementationClass(Class clazz) {
+      return clazz.isAnnotationPresent(ScenarioScoped.class)
+          || Arrays.stream(clazz.getDeclaredMethods()).anyMatch(method ->
+              Arrays.stream(method.getAnnotations())
+                      .anyMatch(annotation ->
+                              isHookAnnotation(annotation) || isStepDefinitionAnnotation(annotation)
+                      ));
+  }
+
+  private boolean isHookAnnotation(Annotation annotation) {
+    Class annotationType = annotation.annotationType();
+    return annotationType.equals(Before.class) || annotationType.equals(After.class);
+  }
+
+  private boolean isStepDefinitionAnnotation(Annotation annotation) {
+    Class annotationType = annotation.annotationType();
+    return annotationType.getAnnotation(StepDefAnnotation.class) != null;
+  }
+
 }
