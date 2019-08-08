9c3f61847b067aef98ed14a62242902f9404561f
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2019-08-08 12:40:51 +0200

    Travis updated to work with BrowserStack

--- run_browserstack_tests.sh
+++ run_browserstack_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean install -Dwebdriver.type=remote -Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub -Dwebdriver.cap.browser=$BROWSER -Dwebdriver.cap.os="${OS}" -Dwebdriver.cap.browserstack.localIdentifier="${BROWSERSTACK_LOCAL_IDENTIFIER}"
+mvn clean install -Dwebdriver.type=remote -Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub -Dwebdriver.cap.browser=$BROWSER -Dwebdriver.cap.os="${OS}" -Dwebdriver.cap.os_version="${OS_VER}" -Dwebdriver.cap.browserstack.localIdentifier="${BROWSERSTACK_LOCAL_IDENTIFIER}"
