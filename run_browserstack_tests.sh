c4e6cea7fc0dd8e4b1a9d35d366f16af12ca0118
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2019-08-19 17:41:49 +0200

    Travis build updated to include Appium tests

--- run_browserstack_tests.sh
+++ run_browserstack_tests.sh
@@ -2,7 +2,7 @@
 
 ARGS=()
 [[ -z "$DEVICE" ]] && ARGS+=("-Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub")
-[[ ! -z "$BROWSERSTACK_LOCAL_IDENTIFIER" ]] && ARGS+=('-Dwebdriver.cap.browserstack.localIdentifier="${BROWSERSTACK_LOCAL_IDENTIFIER}"')
+[[ ! -z "$BROWSERSTACK_LOCAL_IDENTIFIER" ]] && ARGS+=("-Dwebdriver.cap.browserstack.localIdentifier=${BROWSERSTACK_LOCAL_IDENTIFIER}")
 [[ ! -z "$BROWSER" ]] && ARGS+=("-Dwebdriver.cap.browser=$BROWSER")
 [[ ! -z "$WEBDRIVER_TYPE" ]] && ARGS+=("-Dwebdriver.type=$WEBDRIVER_TYPE") || ARGS+=('-Dwebdriver.type=remote')
 [[ ! -z "$OS" ]] && ARGS+=("-Dwebdriver.cap.os=${OS}")
